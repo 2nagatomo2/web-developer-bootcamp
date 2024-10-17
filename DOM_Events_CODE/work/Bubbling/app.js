1b76b198a6da71057e7638ba463c4082c4eb5ce6
2nagatomo2 <2nagatomo2@gmail.com>
2024-10-17 23:32:54 +0900

    10/17 add Bubbling

---
+++ DOM_Events_CODE/work/Bubbling/app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("#changeColor");
+const container = document.querySelector("#container");
+
+button.addEventListener("click", function (e) {
+  // stopPropagation を加えると bubbling が止まる。
+  e.stopPropagation();
+  container.style.backgroundColor = makeRandomColor();
+});
+
+container.addEventListener("click", function () {
+  container.classList.add("hide");
+});
+
+const makeRandomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
